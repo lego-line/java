9dd4db28050de02b17e864e9f736a8dc5f2ee6cd
Eric Wieser <wieser.eric@gmail.com>
2015-08-20 15:59:38 +0100

    Add software for junction light sensors

--- remote/legoline/ui/JunctionPanel.java
+++ remote/legoline/ui/JunctionPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import javax.swing.*;
 
 import legoline.hardware.Junction;
+import legoline.hardware.ObjectDetector;
 
 public class JunctionPanel extends JPanel {
 	private static final long serialVersionUID = -7651630967299290601L;
@@ -36,9 +37,57 @@ public class JunctionPanel extends JPanel {
 			});
 			add(button);
 		}
+		
+
+		final JLabel sideInSensorLabel = new JLabel("Side in:");
+		add(sideInSensorLabel);
+		final JLabel sideInSensorReading = new JLabel();
+		add(sideInSensorReading);
+		final JCheckBox sideInSensorPresent = new JCheckBox("pallet?");
+		sideInSensorPresent.setEnabled(false);
+		add(sideInSensorPresent);
+		
+		final JLabel mainInSensorLabel = new JLabel("Main in:");
+		add(mainInSensorLabel);
+		final JLabel mainInSensorReading = new JLabel();
+		add(mainInSensorReading);
+		final JCheckBox mainInSensorPresent = new JCheckBox("pallet?");
+		mainInSensorPresent.setEnabled(false);
+		add(mainInSensorPresent);
+
+		final JLabel outSensorLabel = new JLabel("Out:");
+		add(outSensorLabel);
+		final JLabel outSensorReading = new JLabel();
+		add(outSensorReading);
+		final JCheckBox outSensorPresent = new JCheckBox("pallet?");
+		outSensorPresent.setEnabled(false);
+		add(outSensorPresent);
+
+		new Timer(50, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try{
+					boolean palletSideIn = junc.sideInSensor.hasObject();
+					boolean palletMainIn = junc.mainInSensor.hasObject();
+					boolean palletOut = junc.outSensor.hasObject();
+					sideInSensorPresent.setSelected(palletSideIn);
+					mainInSensorPresent.setSelected(palletMainIn);
+					outSensorPresent.setSelected(palletOut);
+					
+					ObjectDetector.ReadingPair sideIn = junc.sideInSensor.getLastReading();
+					ObjectDetector.ReadingPair mainIn = junc.sideInSensor.getLastReading();
+					ObjectDetector.ReadingPair out = junc.outSensor.getLastReading();
+					sideInSensorReading.setText(sideIn.lit + ", " + sideIn.unlit);
+					mainInSensorReading.setText(mainIn.lit + ", " + mainIn.unlit);
+					outSensorReading.setText(out.lit + ", " + out.unlit);
+				}
+				catch(InterruptedException ie) { }
+			}
+		}).start();
+		
+		
 		JPanel side = new BeltPanel(junc.sideBelt, "sideline");
 		JPanel main = new BeltPanel(junc.mainBelt, "mainline");
-		
 		add(side);
 		add(main);
 		
@@ -52,12 +101,46 @@ public class JunctionPanel extends JPanel {
 				.addComponent(side)
 				.addComponent(main)
 			)
+			.addGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup()
+					.addComponent(sideInSensorLabel)
+					.addComponent(mainInSensorLabel)
+					.addComponent(outSensorLabel)
+				)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(sideInSensorReading)
+					.addComponent(mainInSensorReading)
+					.addComponent(outSensorReading)
+				)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(sideInSensorPresent)
+					.addComponent(mainInSensorPresent)
+					.addComponent(outSensorPresent)
+				)
+			)
 		);
 		layout.setVerticalGroup(layout.createParallelGroup()
 			.addComponent(button, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 			.addGroup(layout.createSequentialGroup()
 				.addComponent(side)
 				.addComponent(main)
+			)
+			.addGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup()
+					.addComponent(sideInSensorLabel)
+					.addComponent(sideInSensorReading)
+					.addComponent(sideInSensorPresent)
+				)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(mainInSensorLabel)
+					.addComponent(mainInSensorReading)
+					.addComponent(mainInSensorPresent)
+				)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(outSensorLabel)
+					.addComponent(outSensorReading)
+					.addComponent(outSensorPresent)
+				)
 			)
 		);
 	}
--- shared/legoline/hardware/Junction.java
+++ shared/legoline/hardware/Junction.java
@@ -11,6 +11,10 @@ public class Junction implements AutoCloseable {
 	
 	public RegulatedMotor pusher;
 	public EncoderMotor pusherRaw;
+
+	public ObjectDetector sideInSensor;
+	public ObjectDetector mainInSensor;
+	public ObjectDetector outSensor;
 	
 	private NXTCommand conn;
 	private boolean isZeroed = false;
@@ -24,6 +28,9 @@ public class Junction implements AutoCloseable {
 	public Junction(NXTCommand conn) {
 		RegulatedMotor sideBeltMotor;
 		RegulatedMotor mainBeltMotor;
+		ADSensorPort sideInPort;
+		ADSensorPort mainInPort;
+		ADSensorPort outPort;
 	
 		// switch between remote and local cases
 		if(conn == null) {
@@ -31,16 +38,28 @@ public class Junction implements AutoCloseable {
 			pusher = Motor.B;
 			pusherRaw = new NXTMotor(MotorPort.B);
 			mainBeltMotor = Motor.C;
+			
+			sideInPort = SensorPort.S1;
+			mainInPort = SensorPort.S2;
+			outPort = SensorPort.S3;
 		}
 		else {
 			sideBeltMotor = new RemoteMotor(conn, 0);
 			pusher = new RemoteMotor(conn, 1);
 			pusherRaw =  new NXTMotor(new RemoteMotorPort(conn, 1));
 			mainBeltMotor = new RemoteMotor(conn, 2);
+			
+			sideInPort = new RemoteSensorPort(conn, 0);
+			mainInPort = new RemoteSensorPort(conn, 1);
+			outPort = new RemoteSensorPort(conn, 2);
 		}
 		
 		sideBelt = new Belt(MirrorMotor.invertMotor(sideBeltMotor), 21);  // port C
 		mainBelt = new Belt(MirrorMotor.invertMotor(mainBeltMotor), 27);  // port C
+
+		sideInSensor = ObjectDetector.upwardsFacing(new LightSensor(sideInPort));
+		mainInSensor = ObjectDetector.upwardsFacing(new LightSensor(mainInPort));
+		outSensor = ObjectDetector.upwardsFacing(new LightSensor(outPort));
 		
 		this.conn = conn;
 
